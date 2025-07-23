37dcebc1b52cd2f96c0b32bde99e1231f49fe901
Suman J K <sumanjk35@gmail.com>
2025-07-23 15:30:24 +0530

    Update suman_springboot_app.java

--- src/main/java/com/suman/suman_springboot_app.java
+++ src/main/java/com/suman/suman_springboot_app.java
@@ -2,10 +2,17 @@ package com.suman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class suman_springboot_app {
+public class suman_springboot_app extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(suman_springboot_app.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(suman_springboot_app.class);
 	}
 }
