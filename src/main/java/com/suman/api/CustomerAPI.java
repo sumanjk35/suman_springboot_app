bf1604a921db23fa10e6e14d33bb0e894086e422
Suman J K <sumanjk35@gmail.com>
2025-07-23 14:52:39 +0530

    Update CustomerAPI.java

--- src/main/java/com/suman/api/CustomerAPI.java
+++ src/main/java/com/suman/api/CustomerAPI.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.suman.dto.CustomerDTO;
 import com.suman.exception.InfyBankException;
 import com.suman.service.CustomerService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/sumanbank")
 public class CustomerAPI {
